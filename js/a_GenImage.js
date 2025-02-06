79182b9cd8da520b9833c3da6cd1e22b1fb1dd4f
pouyy67yu <johnson.prana@gmail.com>
2025-02-07 00:15:57 +0100

    Delete js directory

--- js/a_GenImage.js
+++
@@ -1,6 +0,0 @@
-function GenImage(self) {
-    var s = $(self);
-    $.get(srcGet + 'Files/Handlers/GenImage.ashx?q=' + s.attr('src'), function(d) {
-        s.attr('src', d).css('border-radius', '50%')
-    }, "text")
-}
